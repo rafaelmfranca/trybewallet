e29da9b92f7b0d15a829b98f02f013b1cf24f727
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-02-28 20:03:10 -0300

    :sparkles: Initial config

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,16 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+  case LOGIN:
+    return { ...state, ...payload };
+  default:
+    return state;
+  }
+};
+
+export default user;
