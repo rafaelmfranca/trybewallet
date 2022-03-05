8c70d41f8aa77d5051c5598fe0cf4b67104f25a9
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-03-05 15:16:56 -0300

    :lipstick: Added global css reset component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store/store';
+import Reset from './styles/generic/Reset';
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={ store }>
+      <Reset />
       <App />
     </Provider>
   </BrowserRouter>,
